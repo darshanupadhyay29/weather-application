602c88ab508e038f3d2a0a364afa1d5a090eb349
Darshan Upadhyay <119104916+darshanupadhyay29@users.noreply.github.com>
2024-04-23 00:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-// const weather = {
-//     apiKey: "1f78fecdf25f9bbc6b757b5d3345bf52",
-// };
-
-// getWeatherData();
-
-// function getWeatherData() {
-//     navigator.geolocation.getCurrentPosition((success) => {
-//         console.log(success);
-        
-//         let { latitude, longitude } = success.coords;
-        
-//         fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&appid=${weather.apiKey}`)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data)
-//         })
-//     });
-// };
-
-
-// displayWeather : function() {
-//     const { name } = data;
-//     const { temp,pressure } = data.main;
-//     const { speed } = data.wind;
-
-//     console.log(name,temp,pressure,speed);
-// }
-
-
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '2b092066admshbab10c5ee0574f2p1d15cfjsne6a60d83986f',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com',
-    }
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2b092066admshbab10c5ee0574f2p1d15cfjsne6a60d83986f",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
-const getWeather = (city) => {
-    cityName.innerHTML = city
 
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            temp.innerHTML = response.temp
-            wind_speed.innerHTML = response.wind_speed
-            humidity.innerHTML = response.humidity
-            cloud_pct.innerHTML = response.cloud_pct
-        })
-        .catch(err => console.error(err));
-}
+const getWeather = (latitude, longitude) => {
+  fetch(
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?lat=${latitude}&lon=${longitude}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+     // Update HTML elements with weather information
+      cityName.innerHTML = "Weather in " + response.city_name; // Update city name
+      temp.innerHTML = response.temp;
+      wind_speed.innerHTML = response.wind_speed;
+      humidity.innerHTML = response.humidity;
+      cloud_pct.innerHTML = response.cloud_pct;
+    })
+    .catch((err) => console.error(err));
+};
 
+// Function to get current location
+const getCurrentLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        getWeather(latitude, longitude);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+};
 
-
+// Event listener for button click to get weather for current location
 btn.addEventListener("click", (e) => {
-    e.preventDefault()
-    getWeather(city.value)
-})
-getWeather("Delhi")
+  e.preventDefault();
+  getCurrentLocation();
+});
 
-    
+// Call getCurrentLocation() initially to get weather for current location
+getCurrentLocation();
